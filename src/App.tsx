fb712a0d734404b9f061ebbfeb6b2196a677a00a
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2023-01-13 21:06:55 +0100

    Use text URL parameter

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
-import { FC } from "react";
+import React, { FC, useMemo } from "react";
 import styled from "styled-components";
 import { LEDMessageSign } from "@gunnarbirnir/led-message-sign";
 
+const DEFAULT_TEXT = "LED Message Sign";
+
 const App: FC = () => {
+  const messageText = useMemo(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const initText = urlParams.get("text");
+
+    return initText ? decodeURIComponent(initText) : DEFAULT_TEXT;
+  }, []);
+
+  if (!messageText) {
+    return null;
+  }
+
   return (
     <Container>
-      <LEDMessageSign
-        text="LED Message Sign"
-        // height={200}
-        // width={1000}
-        // fullWidth
-        // colorHue={180}
-        // multiColor
-        // hideFrame
-        // coloredOffLights={false}
-        // animationFramesPerUpdate={20}
-      />
+      <InnerContainer>
+        <LEDMessageSign
+          text={messageText}
+          height={180}
+          // width={1000}
+          fullWidth
+          // colorHue={180}
+          multiColor
+          // hideFrame
+          // coloredOffLights={false}
+          // animationFramesPerUpdate={20}
+        />
+      </InnerContainer>
     </Container>
   );
 };
@@ -25,11 +40,14 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 20px;
   height: 100vh;
   width: 100vw;
   background-color: hsl(0deg 0% 0%);
-  color: hsl(0deg 0% 100%);
+`;
+
+const InnerContainer = styled.div`
+  width: 100%;
+  max-width: 1000px;
 `;
 
 export default App;
